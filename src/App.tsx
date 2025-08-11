f6fd5819321735a97094b12edd39460c84996911
Rafael <rafaelviniciussilvarosa@gmail.com>
2025-08-11 18:49:58 -0300

    att estrutura base

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
-import { Heading } from "./components/Heading";
-
 import "./styles/theme.css";
 import "./styles/global.css";
 
 export function App() {
-  console.log("oi");
-
   return (
     <>
-       <Heading/>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, hic.
-        Molestias iusto sit, molestiae ex veritatis earum labore. Necessitatibus
-        sunt vel voluptate amet quas, blanditiis minus ipsa velit beatae
-        nostrum!
-      </p>
+      <div className="container">
+        <div className="content">
+          <section>Logo</section>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <section>menu</section>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <section>formulário</section>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <section>footer</section>
+        </div>
+      </div>
     </>
   );
 }
