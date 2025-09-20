0529889d832c854bf4bdb9bf36472a37e07e488f
Rafael <rafaelviniciussilvarosa@gmail.com>
2025-09-19 22:20:01 -0300

    web worker com setTimeout para contagem regressiva

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -16,7 +16,7 @@ export function MainForm() {
 
   const nextCycle = getNextCycle(state.currentCycle);
   const nextCycleType = getNextCycleType(nextCycle);
-  
+
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
