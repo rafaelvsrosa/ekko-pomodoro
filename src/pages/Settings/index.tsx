2c06fcc98b887c1a82f51c922094dedb11dac7c9
Rafael <rafaelviniciussilvarosa@gmail.com>
2025-10-15 23:55:40 -0300

    formularios do settings

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -4,8 +4,50 @@ import { DefaultButton } from "../../components/DefaultButton";
 import { DefaultInput } from "../../components/DefaultInput";
 import { Heading } from "../../components/Heading";
 import { MainTemplate } from "../../templates/MainTemplate";
+import { useRef } from "react";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { showMessage } from "../../adapters/showMessage";
 
 export function Settings() {
+  const { state } = useTaskContext();
+  const workTimeInput = useRef<HTMLInputElement>(null);
+  const shortBreakTimeInput = useRef<HTMLInputElement>(null);
+  const longBreakTimeInput = useRef<HTMLInputElement>(null);
+
+  function handleSaveSettings(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    showMessage.dissmisss();
+
+    const formErrors = [];
+
+    const workTime = Number(workTimeInput.current?.value);
+    const shortBreakTime = Number(shortBreakTimeInput.current?.value);
+    const longBreakTime = Number(longBreakTimeInput.current?.value);
+
+    if (isNaN(workTime) || isNaN(shortBreakTime) || isNaN(longBreakTime)) {
+      formErrors.push("Digite apenas números para os campos");
+    }
+
+      if (workTime < 1 || workTime > 99) {
+      formErrors.push('Digite apenas valores entre 1 a 99 para Foco')
+    }
+
+     if (shortBreakTime < 1 || shortBreakTime > 30) {
+      formErrors.push('Digite apenas valores entre 1 a 30 para Descanso Curto')
+    }
+
+      if (longBreakTime < 1 || longBreakTime > 60) {
+      formErrors.push('Digite apenas valores entre 1 a 60 para Descanso Longo')
+    }
+
+    if (formErrors.length > 0) {
+      formErrors.forEach((error) => {
+        showMessage.error(error);
+      });
+      return;
+    }
+  }
+
   return (
     <MainTemplate>
       <Container>
@@ -20,14 +62,32 @@ export function Settings() {
       </Container>
 
       <Container>
-        <form action="" className="form">
+        <form onSubmit={handleSaveSettings} action="" className="form">
           <div className="formRow">
-            <DefaultInput id="workTime" labelText="Foco" />
+            <DefaultInput
+              id="workTime"
+              labelText="Foco"
+              ref={workTimeInput}
+              defaultValue={state.config.workTime}
+              type="number"
+            />
           </div>
           <div className="formRow">
-            <DefaultInput id="shortBreakTime" labelText="Descanso curto" />
+            <DefaultInput
+              id="shortBreakTime"
+              labelText="Descanso curto"
+              ref={shortBreakTimeInput}
+              defaultValue={state.config.shortBreakTime}
+              type="number"
+            />
             <div className="formRow">
-              <DefaultInput id="longBreakTime" labelText="Descanso longo" />
+              <DefaultInput
+                id="longBreakTime"
+                labelText="Descanso longo"
+                ref={longBreakTimeInput}
+                defaultValue={state.config.longBreakTime}
+                type="number"
+              />
               <div className="formRow">
                 <DefaultButton
                   icon={<SaveIcon />}
